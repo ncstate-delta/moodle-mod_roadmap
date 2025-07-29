0aa7659c36adcb725bf840ca86208b3b2b782240
Steve Bader <smbader@ncsu.edu>
2025-07-29 00:29:40 -0400

    configuration: change over all on click events to a single bind. (#8)

--- amd/src/roadmap_config.js
+++ amd/src/roadmap_config.js
@@ -92,55 +92,6 @@ define(['jquery',
                 .then(function(html, js) {
                     templates.prependNodeContents(self.inputSelector, html, js);
 
-                    $('#add-phase').click(function(e) {
-                        RoadmapConfig.prototype.addPhase(e);
-                    });
-                    $('.add-phase-cycle').click(function(e) {
-                        RoadmapConfig.prototype.addCycle(e);
-                    });
-                    $('.add-cycle-step').click(function(e) {
-                        RoadmapConfig.prototype.addStep(e);
-                    });
-
-                    $('.phase-collapse-control').click(function(e) {
-                        RoadmapConfig.prototype.collapsePhase(e);
-                    });
-                    $('.cycle-collapse-control').click(function(e) {
-                        RoadmapConfig.prototype.collapseCycle(e);
-                    });
-                    $('.step-collapse-control').click(function(e) {
-                        RoadmapConfig.prototype.collapseStep(e);
-                    });
-
-                    $('.phase-delete-control').click(function(e) {
-                        RoadmapConfig.prototype.deletePhase(e);
-                    });
-                    $('.cycle-delete-control').click(function(e) {
-                        RoadmapConfig.prototype.deleteCycle(e);
-                    });
-                    $('.step-delete-control').click(function(e) {
-                        RoadmapConfig.prototype.deleteStep(e);
-                    });
-
-                    $('.phase-up-control').click(function(e) {
-                        RoadmapConfig.prototype.upPhase(e);
-                    });
-                    $('.phase-down-control').click(function(e) {
-                        RoadmapConfig.prototype.downPhase(e);
-                    });
-                    $('.cycle-up-control').click(function(e) {
-                        RoadmapConfig.prototype.upCycle(e);
-                    });
-                    $('.cycle-down-control').click(function(e) {
-                        RoadmapConfig.prototype.downCycle(e);
-                    });
-                    $('.step-up-control').click(function(e) {
-                        RoadmapConfig.prototype.upStep(e);
-                    });
-                    $('.step-down-control').click(function(e) {
-                        RoadmapConfig.prototype.downStep(e);
-                    });
-
                     $('.expand-collapse-controls .collapse_everything').click(function(e) {
                         RoadmapConfig.prototype.collapseEverything(e);
                     });
@@ -150,7 +101,7 @@ define(['jquery',
 
                     // Instead of binding and unbinding specific elements over and over as they are created,
                     // This event will capture all clicks, always and based on data we can determine the action.
-                    $('#roadmapconfiguration').click(RoadmapConfig.prototype.clickHandler);
+                    $('#roadmapconfiguration a[data-action]').click(RoadmapConfig.prototype.clickHandler);
 
                     RoadmapConfig.prototype.phaseColorChange($('select[name="phasecolorpattern"]'));
 
@@ -175,8 +126,8 @@ define(['jquery',
         };
 
         RoadmapConfig.prototype.clickHandler = function(event) {
-            var thisnode = $(event.target);
-            var action = thisnode.data('action');
+            let thisnode = $(event.currentTarget);
+            let action = thisnode.data('action');
 
             if (action == 'collapse_all_phases') {
                 RoadmapConfig.prototype.collapsePhases();
@@ -194,6 +145,36 @@ define(['jquery',
             } else if (action == 'expand_all_steps') {
                 RoadmapConfig.prototype.expandSteps(thisnode.parent('.step-container-controls')
                     .next('.cycle-steps-container'));
+            } else if (action == 'phase_collapse_control') {
+                RoadmapConfig.prototype.collapsePhase(thisnode);
+            } else if (action == 'cycle_collapse_control') {
+                RoadmapConfig.prototype.collapseCycle(thisnode);
+            } else if (action == 'step_collapse_control') {
+                RoadmapConfig.prototype.collapseStep(thisnode);
+            } else if (action == 'phase_delete_control') {
+                RoadmapConfig.prototype.deletePhase(thisnode);
+            } else if (action == 'cycle_delete_control') {
+                RoadmapConfig.prototype.deleteCycle(thisnode);
+            } else if (action == 'step_delete_control') {
+                RoadmapConfig.prototype.deleteStep(thisnode);
+            } else if (action == 'add_phase') {
+                RoadmapConfig.prototype.addPhase();
+            } else if (action == 'phase_up_control') {
+                RoadmapConfig.prototype.upPhase(thisnode);
+            } else if (action == 'phase_down_control') {
+                RoadmapConfig.prototype.downPhase(thisnode);
+            } else if (action == 'add_phase_cycle') {
+                RoadmapConfig.prototype.addCycle(thisnode);
+            } else if (action == 'cycle_up_control') {
+                RoadmapConfig.prototype.upCycle(thisnode);
+            } else if (action == 'cycle_down_control') {
+                RoadmapConfig.prototype.downCycle(thisnode);
+            } else if (action == 'add_cycle_step') {
+                RoadmapConfig.prototype.addStep(thisnode);
+            } else if (action == 'step_up_control') {
+                RoadmapConfig.prototype.upStep(thisnode);
+            } else if (action == 'step_down_control') {
+                RoadmapConfig.prototype.downStep(thisnode);
             }
         };
 
@@ -247,10 +228,7 @@ define(['jquery',
             }));
         };
 
-        RoadmapConfig.prototype.addPhase = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
+        RoadmapConfig.prototype.addPhase = function() {
             var config = JSON.parse($('input[name="roadmapconfiguration"]').val());
             var nextIndex = config.phases.length;
             var maxPhaseId = parseInt(RoadmapConfig.prototype.maxValue('phase'));
@@ -261,47 +239,13 @@ define(['jquery',
             templates.render('mod_roadmap/configuration_phase', newPhase)
                 .then(function(html, js) {
                     templates.appendNodeContents('#phase-container', html, js);
-
-                    $('.phase-collapse-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.collapsePhase(e);
-                        });
-
-                    $('.phase-delete-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.deletePhase(e);
-                        });
-
-                    $('.phase-up-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.upPhase(e);
-                        });
-
-                    $('.phase-down-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.downPhase(e);
-                        });
-
-                    $('#add-phase').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.addPhase(e);
-                        });
-
-                    $('.add-phase-cycle').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.addCycle(e);
-                        });
-
                     phasesave.rebindInputs();
                     RoadmapConfig.prototype.bindConfigSave();
                     return null;
                 }).fail(notification.exception);
         };
 
-        RoadmapConfig.prototype.addCycle = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.addCycle = function(thisnode) {
             var phaseContainer = thisnode.closest('.phase-container');
             var cycleContainer = phaseContainer.children('.phase-cycles-container');
 
@@ -313,31 +257,6 @@ define(['jquery',
                 .then(function(html, js) {
                     templates.appendNodeContents(cycleContainer, html, js);
 
-                    $('.cycle-collapse-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.collapseCycle(e);
-                        });
-
-                    $('.cycle-delete-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.deleteCycle(e);
-                        });
-
-                    $('.cycle-up-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.upCycle(e);
-                        });
-
-                    $('.cycle-down-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.downCycle(e);
-                        });
-
-                    $('.add-cycle-step').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.addStep(e);
-                        });
-
                     learningobjectives.refreshChecklists();
                     cyclesave.rebindInputs();
                     phasesave.rebindInputs();
@@ -345,10 +264,7 @@ define(['jquery',
                 }).fail(notification.exception);
         };
 
-        RoadmapConfig.prototype.addStep = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.addStep = function(thisnode) {
             var cycleContainer = thisnode.closest('.cycle-container');
             var stepsContainer = cycleContainer.children('.cycle-steps-container');
 
@@ -367,26 +283,6 @@ define(['jquery',
             templates.render('mod_roadmap/configuration_step', newStep)
                 .then(function(html, js) {
                     templates.appendNodeContents(stepsContainer, html, js);
-
-                    $('.step-collapse-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.collapseStep(e);
-                        });
-
-                    $('.step-delete-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.deleteStep(e);
-                        });
-
-                    $('.step-up-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.upStep(e);
-                        });
-
-                    $('.step-down-control').unbind('click')
-                        .click(function(e) {
-                            RoadmapConfig.prototype.downStep(e);
-                        });
 
                     stepiconselect.rebindButtons();
                     stepactivityselect.rebindButtons();
@@ -426,7 +322,7 @@ define(['jquery',
 
         RoadmapConfig.prototype.expandChildrenClass = function(parentNode, groupingName) {
 
-            parentNode.find('a.' + groupingName + '-collapse-control').each(function() {
+            parentNode.find('a[data-action="' + groupingName + '_collapse_control"]').each(function() {
                 var wrapper = $(this).closest('.' + groupingName + '-wrapper');
                 var container = wrapper.children('.' + groupingName + '-container');
                 if (container.hasClass("hide")) {
@@ -437,7 +333,7 @@ define(['jquery',
 
         RoadmapConfig.prototype.collapseChildrenClass = function(parentNode, groupingName) {
 
-            parentNode.find('a.' + groupingName + '-collapse-control').each(function() {
+            parentNode.find('a[data-action="' + groupingName + '_collapse_control"]').each(function() {
                 var wrapper = $(this).closest('.' + groupingName + '-wrapper');
                 var container = wrapper.children('.' + groupingName + '-container');
                 if (container.hasClass("visible")) {
@@ -446,38 +342,26 @@ define(['jquery',
             });
         };
 
-        RoadmapConfig.prototype.collapsePhase = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.collapsePhase = function(thisnode) {
             var phaseWrapper = thisnode.closest('.phase-wrapper');
             var metadata = phaseWrapper.children('.phase-container');
             ec.expandCollapse(metadata, thisnode);
         };
 
-        RoadmapConfig.prototype.collapseCycle = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.collapseCycle = function(thisnode) {
             var cycleWrapper = thisnode.closest('.cycle-wrapper');
             var metadata = cycleWrapper.children('.cycle-container');
             ec.expandCollapse(metadata, thisnode);
         };
 
-        RoadmapConfig.prototype.collapseStep = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.collapseStep = function(thisnode) {
             var cycleWrapper = thisnode.closest('.step-wrapper');
             var metadata = cycleWrapper.children('.step-container');
             ec.expandCollapse(metadata, thisnode);
         };
 
-        RoadmapConfig.prototype.deletePhase = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
+        RoadmapConfig.prototype.deletePhase = function(thisnode) {
             if (window.confirm("Are you sure you want to delete this Phase?")) {
-                var thisnode = $(event.currentTarget);
                 var phaseWrapper = thisnode.closest('.phase-wrapper');
                 var phaseId = phaseWrapper.data('phaseid');
                 $('#phase-deletes').val($('#phase-deletes').val() + phaseId + ',');
@@ -486,11 +370,8 @@ define(['jquery',
             }
         };
 
-        RoadmapConfig.prototype.deleteCycle = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
+        RoadmapConfig.prototype.deleteCycle = function(thisnode) {
             if (window.confirm("Are you sure you want to delete this Cycle?")) {
-                var thisnode = $(event.currentTarget);
                 var cycleWrapper = thisnode.closest('.cycle-wrapper');
                 var phaseContainer = thisnode.closest('.phase-container');
                 var cycleId = cycleWrapper.data('cycleid');
@@ -500,11 +381,8 @@ define(['jquery',
             }
         };
 
-        RoadmapConfig.prototype.deleteStep = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
+        RoadmapConfig.prototype.deleteStep = function(thisnode) {
             if (window.confirm("Are you sure you want to delete this Step?")) {
-                var thisnode = $(event.currentTarget);
                 var stepWrapper = thisnode.closest('.step-wrapper');
                 var cycleContainer = thisnode.closest('.cycle-container');
                 var stepId = stepWrapper.data('stepid');
@@ -514,11 +392,7 @@ define(['jquery',
             }
         };
 
-        RoadmapConfig.prototype.upPhase = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.upPhase = function(thisnode) {
             var phaseWrapper = thisnode.closest('.phase-wrapper');
             var prevPhaseWrapper = phaseWrapper.prev();
 
@@ -526,11 +400,7 @@ define(['jquery',
             RoadmapConfig.prototype.configSave();
         };
 
-        RoadmapConfig.prototype.downPhase = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.downPhase = function(thisnode) {
             var phaseWrapper = thisnode.closest('.phase-wrapper');
             var nextPhaseWrapper = phaseWrapper.next();
 
@@ -538,11 +408,7 @@ define(['jquery',
             RoadmapConfig.prototype.configSave();
         };
 
-        RoadmapConfig.prototype.upCycle = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.upCycle = function(thisnode) {
             var cycleWrapper = thisnode.closest('.cycle-wrapper');
             var phaseContainer = thisnode.closest('.phase-container');
             var prevCycleWrapper = cycleWrapper.prev();
@@ -553,11 +419,7 @@ define(['jquery',
             }
         };
 
-        RoadmapConfig.prototype.downCycle = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.downCycle = function(thisnode) {
             var cycleWrapper = thisnode.closest('.cycle-wrapper');
             var phaseContainer = thisnode.closest('.phase-container');
             var nextCycleWrapper = cycleWrapper.next();
@@ -568,11 +430,7 @@ define(['jquery',
             }
         };
 
-        RoadmapConfig.prototype.upStep = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.upStep = function(thisnode) {
             var stepWrapper = thisnode.closest('.step-wrapper');
             var cycleContainer = thisnode.closest('.cycle-container');
             var prevStepWrapper = stepWrapper.prev();
@@ -583,11 +441,7 @@ define(['jquery',
             }
         };
 
-        RoadmapConfig.prototype.downStep = function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-
-            var thisnode = $(event.currentTarget);
+        RoadmapConfig.prototype.downStep = function(thisnode) {
             var stepWrapper = thisnode.closest('.step-wrapper');
             var cycleContainer = thisnode.closest('.cycle-container');
             var nextStepWrapper = stepWrapper.next();
